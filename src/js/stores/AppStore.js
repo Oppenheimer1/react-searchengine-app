541424cdfac51f10d56ec8af252d3a693eb0d3ea
LARS BERG <larsgbr@gmail.com>
2017-10-23 19:48:55 -0500

    first commit, created application and built out most of the functionality

---
+++ src/js/stores/AppStore.js
@@ -0,0 +1,46 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var AppConstants = require('../constants/AppConstants');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+var AppAPI = require('../utils/AppAPI.js');
+
+var CHANGE_EVENT = 'change';
+
+var _items = [];
+var _searchText = '';
+
+var AppStore = assign({}, EventEmitter.prototype, {
+	setSearchText: function(search){
+		_searchText = search.text;
+	},
+	emitChange: function(){
+		this.emit(CHANGE_EVENT);
+	},
+	addChangeListener: function(callback){
+		this.on('change', callback);
+	},
+	removeChangeListener: function(callback){
+		this.removeListener('change', callback);
+	}
+});
+
+AppDispatcher.register(function(payload){
+	var action = payload.action;
+
+	switch(action.actionType){
+		case AppConstants.SEARCH_TEXT:
+			AppAPI.searchText(action.search);
+			AppStore.setSearchText(action.search);
+			AppStore.emit(CHANGE_EVENT);
+			break;
+		case AppConstants.RECEIVE_RESULTS:
+			AppAPI.searchText(action.search);
+			AppStore.setSearchText(action.search);
+			AppStore.emit(CHANGE_EVENT);
+			break;
+	}
+
+	return true;
+});
+
+module.exports = AppStore;
